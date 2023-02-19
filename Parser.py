1098c957702a7f311aa5fa91c816f9d3c53820d0
borzuy2282 <111884651+borzuy2282@users.noreply.github.com>
2023-02-19 15:31:44 +0100

    Add files via upload
    Added new funcs: parsing url and parsing titles

---
+++ Parser.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as b
+
+URL = 'https://petition.president.gov.ua/?status=active&sort=votes&order=desc'
+def parseTitle(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    petitions = soup.find_all('a', class_='pet_link')
+    return [c.text for c in petitions]
+def parseUrl(url):
+    r = requests.get(url)
+    soup = b(r.text, 'html.parser')
+    petitions = soup.find_all('a', class_='pet_link')
+    return [str(c)[36:42] for c in petitions]
+print(*parseTitle(URL), sep='\n')
+print(*parseUrl(URL), sep='\n')
